ec2a508180179f2c68220302167d9fe2700c2ce4
Frida010 <>
2023-10-15 11:22:41 +0000

    add

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,3 +1,4 @@
+from taskmanager import routes  # noqas
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -10,5 +11,3 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
-
-from taskmanager import routes  # noqa
